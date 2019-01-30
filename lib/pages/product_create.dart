e94f6d4e54730a475790298da6cd6836b0a3121a
Engin Yapici <enginyapici@gmail.com>
2019-01-30 01:20:57 -0800

    End of Lecture 88: Showing user input in a text box

--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -1,18 +1,28 @@
 import 'package:flutter/material.dart';
 
-class ProductCreatePage extends StatelessWidget {
+class ProductCreatePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _ProductCreatePageState();
+  }
+}
+
+class _ProductCreatePageState extends State<ProductCreatePage> {
+  String titleValue = '';
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: RaisedButton(
-          child: Text('Save'),
-          onPressed: () {
-            showModalBottomSheet(
-                context: context,
-                builder: (BuildContext context) {
-                  return Center(child: Text('This is a modal!'));
-                });
-          }),
+    return Column(
+      children: <Widget>[
+        TextField(
+          onChanged: (String value) {
+            setState(() {
+              titleValue = value;
+            });
+          },
+        ),
+        Text(titleValue)
+      ],
     );
   }
 }
