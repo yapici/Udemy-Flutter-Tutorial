31d6422ade2ecaf0a10512e45a6ee8d4275618e3
Engin Yapici <enginyapici@gmail.com>
2019-02-14 23:07:28 -0800

    End of Lecture 155: Add favorite button functionality

--- lib/scoped-models/products.dart
+++ lib/scoped-models/products.dart
@@ -36,6 +36,20 @@ class ProductsModel extends Model {
     _selectedProductIndex = null;
   }
 
+  void toggleProductFavoriteStatus() {
+    final bool isCurrentlyFavorite = selectedProduct.isFavorite;
+    final bool newFavoriteStatus = !isCurrentlyFavorite;
+    final Product updatedProduct = Product(
+        title: selectedProduct.title,
+        description: selectedProduct.description,
+        price: selectedProduct.price,
+        image: selectedProduct.image,
+        isFavorite: newFavoriteStatus);
+    _products[_selectedProductIndex] = updatedProduct;
+    _selectedProductIndex = null;
+    notifyListeners(); // Needed to update the product (i.e., re-calls the ScopedModelDescendant 'builder' methods)
+  }
+
   void selectProduct(int index) {
     _selectedProductIndex = index;
   }
