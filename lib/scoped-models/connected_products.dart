c7689aecc0e69bac6f8c0316f68d0bbb2a1f9cd5
Engin Yapici <enginyapici@gmail.com>
2019-03-05 04:27:24 -0800

    End of Lecture 206: Allow editing for own posts only

--- lib/scoped-models/connected_products.dart
+++ lib/scoped-models/connected_products.dart
@@ -163,7 +163,7 @@ mixin ProductsModel on ConnectedProductsModel {
     });
   }
 
-  Future<Null> fetchProducts() {
+  Future<Null> fetchProducts({onlyForUser = false}) {
     _isLoading = true;
     notifyListeners();
 
@@ -198,7 +198,12 @@ mixin ProductsModel on ConnectedProductsModel {
         fetchedProductList.add(product);
       });
 
-      _products = fetchedProductList;
+      _products = onlyForUser
+          ? fetchedProductList.where((Product product) {
+              return product.userId == _authenticatedUser.id;
+            }).toList()
+          : fetchedProductList;
+
       _isLoading = false;
       notifyListeners();
       _selectedProductId = null;
